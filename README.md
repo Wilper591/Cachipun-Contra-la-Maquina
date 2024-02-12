d8ef84bef4957671a7cbd53c01c521327b200a86
Jorge Cerda <jorge_cerda_591@hotmail.com>
2024-02-12 12:22:17 -0300

    Se carga juego cachipun

---
+++ README.md
@@ -0,0 +1,24 @@
+Desarrollar juego cachipun (piedra, papel o tijeras)
+Que se pueda jugar contra el computador y se pueda indicar cuentas veces se desea jugar o repetir el juego.
+Cada uno puede elegir la opcion de forma secreta y compararla, para determinar quien gana.
+Reglas:
+● Tijera le gana a papel
+● Papel le gana a piedra
+● Piedra le gana a tijera
+● Si ambos jugadores eligen la misma opción es un empate
+
+1) Solicitar cuantas veces debe repetirse el juego consecutivamente. Se deben mostrar los resultados y luego pedir respuesta segun lo que haya solicitado el jugador.
+
+2) Solicitar jugada al usuario
+● Piedra
+● Papel 
+● Tijera
+
+3) Generar una jugada automatica de la maquina usando math.random() de js
+
+4) Definir a un ganador considerando la jugada del usuario y la generada automaticamente para la maquina
+
+5) Indicar el resultado de la partida dependiendo del caso
+● Felicitar al ganador en caso de ser el usuario.
+● Indicarle al usuario que ha perdido contra la máquina en caso de que ésta sea la que gane.
+● Declarar un empate.
---
+++ assets/js/index.js
@@ -0,0 +1,74 @@
+function jugar() {
+  let cantidadJuegos = parseInt(prompt("¿Cuantas veces quieres jugar?"));
+  let opciones = ["piedra", "papel", "tijera"];
+  let resultadoRonda = "";
+  let resultadoFinal = "";
+  let marcadorJugador = "";
+  let marcadorComputadora = "";
+  let i = 0;
+
+  while (i < cantidadJuegos) {
+    /* Jugada del jugador */
+    let opcionJugador = prompt("Elige: piedra, papel o tijera").toLowerCase();
+    while (!opciones.includes(opcionJugador)) {
+      opcionJugador = prompt(
+        "Opción inválida. Elige: piedra, papel o tijera"
+      ).toLowerCase();
+    }
+
+    /* Jugada de la maquina */
+    let opcionComputadora =
+      opciones[Math.floor(Math.random() * opciones.length)];
+    let resultado = "";
+    /* Reglas del juego */
+    if (opcionJugador === opcionComputadora) {
+      resultado = "Es un Empate!";
+    } else if (
+      (opcionJugador === "piedra" && opcionComputadora === "tijera") ||
+      (opcionJugador === "papel" && opcionComputadora === "piedra") ||
+      (opcionJugador === "tijera" && opcionComputadora === "papel")
+    ) {
+      resultado = "Ganaste!";
+      marcadorJugador++;
+    } else {
+      resultado = "La computadora gana!";
+    }
+    /* Alert de resultado inmediato de partida */
+    alert("Juego: " +
+      (i + 1) +
+      ". Seleccionaste: " +
+      opcionJugador +
+      ". La Computadora jugó: " +
+      opcionComputadora +
+      ". " +
+      "\n" +
+      resultado +
+      "." +
+      "\n");
+
+    /* Texto HTML del resumen de partidas */
+    resultadoRonda +=
+      "Juego: " +
+      (i + 1) +
+      ". Seleccionaste: " +
+      opcionJugador +
+      ". La Computadora jugó: " +
+      opcionComputadora +
+      ". " +
+      "\n" +
+      resultado +
+      "." +
+      "\n";
+    document.getElementById("resultado").innerText = resultadoRonda;
+    i++;
+  }
+  /* Resultado final del juego */
+  if (marcadorJugador > marcadorComputadora) {
+    resultadoFinal = "El Jugador gana la partida!";
+  } else if (marcadorJugador < marcadorComputadora) {
+    resultadoFinal = "La computadora gana la partida";
+  } else {
+    resultadoFinal = "La partida es un Empate!!";
+  }
+  document.getElementById("resultadoFinal").innerText = resultadoFinal;
+}
