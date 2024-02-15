e187a0e6695dbb49ceef00129e1296dae38c822a
Jorge Cerda <jorge_cerda_591@hotmail.com>
2024-02-15 14:31:02 -0300

    Aplicando correcciones en estructura de la funcion

--- assets/js/borrador.js
+++ assets/js/borrador.js
@@ -1,34 +1,54 @@
-let btnJugar = document.getElementById("btnJugar");
+const btnJugar = document.getElementById("btnJugar");
 btnJugar.addEventListener("click", function (e) {
   e.preventDefault();
   jugar();
 });
-function jugada() {
+const piedra = document.getElementById("btnPiedra");
+const papel = document.getElementById("btnPapel");
+const tijera = document.getElementById("btnTijera");
+piedra.addEventListener("click", function (e) {
+  e.preventDefault();
+  jugada(e.target.value);
+});
+papel.addEventListener("click", function (e) {
+  e.preventDefault();
+  jugada(e.target.value);
+});
+tijera.addEventListener("click", function (e) {
+  e.preventDefault();
+  jugada(e.target.value);
+});
 
-}
 function jugar() {
+  /* Variables Globales */
+  let inputJuegos = document.getElementById("inputJuegos").value;
+  let nroJuegos = Number(inputJuegos);
   let opciones = ["piedra", "papel", "tijera"];
   let resultadoRonda = "";
   let resultadoFinal = "";
-  let marcadorJugador = "";
-  let marcadorComputadora = "";
-  let i = 0;
+  let marcadorJugador = 0;
+  let marcadorComputadora = 0;
+  let errorMsj = document.getElementById("error");
+console.log(nroJuegos)
+  if (nroJuegos > 10) {
+    errorMsj.textContent = "Ingresa un número entre 1 y 10";
+    setTimeout(() => {
+      errorMsj.remove();
+    }, 3000);
+    return;
+  }
 
-  /* Evaluador de condición WHILE */
+  /* Bucle FOR que genera la cantidad de partidas ingresadas */
   for (let i = 0; i < nroJuegos; i++) {
     /* Jugada del jugador */
-    /* Jugada del jugador */
-    let opcionJugador = prompt("Elige: piedra, papel o tijera").toLowerCase();
-    while (!opciones.includes(opcionJugador)) {
-      opcionJugador = prompt(
-        "Opción inválida. Elige: piedra, papel o tijera"
-      ).toLowerCase();
-    }
+
+  
     /* Jugada de la maquina */
     let opcionComputadora =
       opciones[Math.floor(Math.random() * opciones.length)];
     /* Resultando de la ronda */
     let resultado = "";
+
     /* Reglas del juego */
     if (opcionJugador === opcionComputadora) {
       resultado = "Esta ronda es un Empate!";
@@ -75,21 +95,19 @@ function jugar() {
   }
   /* Resultado final del juego */
   if (marcadorJugador > marcadorComputadora) {
-    resultadoFinal = "El Jugador gana la partida!";
+    resultadoFinal = "El Jugador gana la partida!!!";
   } else if (marcadorJugador < marcadorComputadora) {
-    resultadoFinal = "La computadora gana la partida";
+    resultadoFinal = "La computadora gana la partida!!";
+  } else if (marcadorJugador === 0 && marcadorComputadora === 0) {
+    resultadoFinal = "";
   } else {
     resultadoFinal = "La partida es un Empate!!";
   }
   document.getElementById("resultadoFinal").innerText = resultadoFinal;
 }
 
-function jugada() {
-  let piedra = document.getElementById("btnPiedra").value;
-  let papel = document.getElementById("btnPapel").value;
-  let tijera = document.getElementById("btnTijera").value;
-
-  console.log(piedra)
-  console.log(papel)
-  console.log(tijera)
+function jugada(opcion) {
+  const opcionJugador = opcion;
+  console.log("Tu opción es", opcion);
+  return opcionJugador;
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -3,48 +3,61 @@ btnJugar.addEventListener("click", function (e) {
   e.preventDefault();
   jugar();
 });
+let opcionElegida = "";
 const piedra = document.getElementById("btnPiedra");
 const papel = document.getElementById("btnPapel");
 const tijera = document.getElementById("btnTijera");
 piedra.addEventListener("click", function (e) {
   e.preventDefault();
   jugada(e.target.value);
+  opcionElegida = e.target.value;
+  console.log("El target es", e.target.value)
+  console.log("La opcion es: ", opcionElegida);
 });
 papel.addEventListener("click", function (e) {
   e.preventDefault();
+  opcionElegida = e.target.value;
   jugada(e.target.value);
+  console.log("El target es", e.target.value);
+  console.log("La opcion es: ", opcionElegida);
 });
 tijera.addEventListener("click", function (e) {
   e.preventDefault();
+  opcionElegida = e.target.value;
   jugada(e.target.value);
+  console.log("El target es", e.target.value);
+  console.log("La opcion es: ", opcionElegida);
 });
-function jugada(opcion) {
-  let opcionJugador = opcion;
-  console.log("Tu opción es", opcion);
-  return resultado;
-}
+
+
 function jugar() {
   let inputJuegos = document.getElementById("inputJuegos").value;
+  let nroJuegos = Number(inputJuegos);
   let opciones = ["piedra", "papel", "tijera"];
   let resultadoRonda = "";
   let resultadoFinal = "";
   let marcadorJugador = 0;
   let marcadorComputadora = 0;
-  let nroJuegos = Number(inputJuegos);
   let errorMsj = document.getElementById("error");
-  if (nroJuegos <= 0) {
-    errorMsj.textContent = "Ingresa un numero entre 1 y 10";
+  console.log(nroJuegos);
+  if (nroJuegos <= 0 || nroJuegos > 10) {
+    errorMsj.textContent = "Ingresa un número entre 1 y 10";
+    setTimeout(() => {
+      errorMsj.remove();
+    }, 3000);
+    return;
   }
-  /* Bucle for que genera la cantidad de partidas ingresadas */
+  /* Bucle FOR que genera la cantidad de partidas ingresadas */
   for (let i = 0; i < nroJuegos; i++) {
     /* Jugada del jugador */
-    opcionJugador = resultado;
-    console.log(opcionJugador);
+
+    let opcionJugador = opcionElegida;
     /* Jugada de la maquina */
     let opcionComputadora =
       opciones[Math.floor(Math.random() * opciones.length)];
     /* Resultando de la ronda */
     let resultado = "";
+
     /* Reglas del juego */
     if (opcionJugador === opcionComputadora) {
       resultado = "Esta ronda es un Empate!";
@@ -59,24 +72,29 @@ function jugar() {
       resultado = "La computadora gana la ronda!";
       marcadorComputadora++;
     }
-    /* Alert de resultado inmediato de partida */
-    alert(
-      "Juego: " +
-        (i + 1) +
-        ". Seleccionaste: " +
-        opcionJugador +
-        ". La Computadora jugó: " +
-        opcionComputadora +
-        ". " +
-        "\n" +
-        resultado +
-        "." +
-        "\n"
-    );
+  }
+  resultado();
+  /* Resultado final del juego */
+  if (marcadorJugador > marcadorComputadora) {
+    resultadoFinal = "El Jugador gana la partida!!!";
+  } else if (marcadorJugador < marcadorComputadora) {
+    resultadoFinal = "La computadora gana la partida!!";
+  } else if (marcadorJugador === 0 && marcadorComputadora === 0) {
+    resultadoFinal = "";
+  } else {
+    resultadoFinal = "La partida es un Empate!!";
+  }
+  document.getElementById("resultadoFinal").innerText = resultadoFinal;
+}
 
-    /* Texto HTML del resumen de partidas */
-    resultadoRonda +=
-      "Juego: " +
+function jugada(opcion) {
+  const opcionJugador = opcion;
+  return opcionJugador;
+}
+function resultado() {
+  /* Alert de resultado inmediato de partida */
+  alert(
+    "Juego: " +
       (i + 1) +
       ". Seleccionaste: " +
       opcionJugador +
@@ -86,18 +104,21 @@ function jugar() {
       "\n" +
       resultado +
       "." +
-      "\n";
-    document.getElementById("resultado").innerText = resultadoRonda;
-  }
-  /* Resultado final del juego */
-  if (marcadorJugador > marcadorComputadora) {
-    resultadoFinal = "El Jugador gana la partida!!!";
-  } else if (marcadorJugador < marcadorComputadora) {
-    resultadoFinal = "La computadora gana la partida!!";
-  } else if (marcadorJugador === 0 && marcadorComputadora === 0) {
-    resultadoFinal = "No hubo partida :(";
-  } else {
-    resultadoFinal = "La partida es un Empate!!";
-  }
-  document.getElementById("resultadoFinal").innerText = resultadoFinal;
+      "\n"
+  );
+
+  /* Texto HTML del resumen de partidas */
+  resultadoRonda +=
+    "Juego: " +
+    (i + 1) +
+    ". Seleccionaste: " +
+    opcionJugador +
+    ". La Computadora jugó: " +
+    opcionComputadora +
+    ". " +
+    "\n" +
+    resultado +
+    "." +
+    "\n";
+  document.getElementById("resultado").innerText = resultadoRonda;
 }
